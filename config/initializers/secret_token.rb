399832d9f41a3ba05389731ae55fd4a10e03b746
Tom Oakes <toaksie@gmail.com>
2012-04-23 18:10:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdrenta::Application.config.secret_token = '9db61f8d46adf101765600ce24fcec41bafbb4a69cfb163e0013245cea9d51133c7b199c7c5e307e814990fd18123cd266bccebdb9f831cdb14c637dd5371c3c'
